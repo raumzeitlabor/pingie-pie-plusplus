622a6e7cff99125b71223b886bef0523c5a0f8f6
Maik Fischer <maikf@qu.cx>
2013-05-07 19:22:18 +0200

    python: autowrap words generated via /create/text

--- python/raumzeitlabor/pingie/display.py
+++ python/raumzeitlabor/pingie/display.py
@@ -41,6 +41,7 @@ GPIO.setup(B7, GPIO.OUT)
 GPIO.setup(PS, GPIO.OUT)
 
 fixed_9x15 = ImageFont.truetype("Fixed9x15.ttf", 15)
+fixed_5x8 = ImageFont.truetype("Fixed5x8.ttf", 8)
 
 def transfer(image):
     # resize image to display size and rotate, because it's mounted upside down
@@ -67,11 +68,31 @@ def transfer(image):
     GPIO.output(PS, GPIO.HIGH)
     GPIO.output(PS, GPIO.LOW)
 
-def render_text(text):
-    pixmap = Image.new('1', (WIDTH, HEIGHT))
-    draw = ImageDraw.Draw(pixmap)
+def render_text(text, font):
+    from textwrap import wrap
+    from math import floor
 
-    draw.rectangle((0, 0, WIDTH, HEIGHT), fill=0)
+    if font == "5x8":
+        font = fixed_5x8
+    elif font == "9x15":
+        font = fixed_9x15
 
-    draw.text((0, 0), text, font=fixed_9x15, fill=1)
-    return pixmap
+    # doesn't matter which character
+    (font_width, font_height) = font.getsize('m')
+
+    # if the text is to large for a display-sized image,
+    # wordwrap the text and use additional lines; enlarge it accordingly
+    line_chars = floor(WIDTH / font_width)
+    text = list(wrap(text, line_chars))
+    lines = len(text)
+
+    image = Image.new('1', (WIDTH, font_height * lines))
+    draw = ImageDraw.Draw(image)
+
+    line = 0
+    for line in range(0, lines):
+        text = text[line]
+        draw.text((0, font_height * line), text, font=font, fill=1)
+        line = line + 1
+
+    return image
